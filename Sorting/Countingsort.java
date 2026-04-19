ac70d2b96b32c64311aadf603ee03cde7c816ee2
Anjali <anjalibhandari9891@gmail.com>
2026-04-19 07:54:26 +0530

    Countingsort.java

---
+++ Sorting/Countingsort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Counting_sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 2, 1, 1, 0, 1, 2, 5, 4, 0, 2, 8, 7, 9, 2, 6, 1, 9 };
+		System.out.println(Arrays.toString(sort(arr)));
+	}
+	public static int [] sort(int [] arr) {
+		int max=Arrays.stream(arr).max().getAsInt();
+		int [] freq=new int [max+1];
+		for (int i = 0; i < arr.length; i++) {
+			freq[arr[i]]++;
+		}
+		for (int i = 1; i < freq.length; i++) {
+			freq[i]=freq[i]+freq[i-1];
+		}
+		int [] ans=new int [arr.length];
+		for (int i = arr.length-1; i >=0; i--) {
+			int x=arr[i];
+			int c=freq[x];
+			ans[c-1]=x;
+			freq[x]--;
+		}
+		return ans;
+	}
+}
